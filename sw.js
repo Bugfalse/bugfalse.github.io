dfab00af8801109f05c0d5222655afbb16fa4684
MalliKarjunaReddy-mkr <gmkr5429@gmail.com>
2026-08-18 08:35:01 +0530

    feat: add lightweight offline shell service worker

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'bugfalse-shell-v1';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './favicon.svg'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
+self.addEventListener('fetch', event => {
+  if (new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  })));
+});
